978de5b3537c3025976c94c9894cb1b2121bb5a6
Jack1Player <chricg99@gmail.com>
2021-05-03 00:14:20 +0530

    sm8250-common: enable debug USB

--- kona.mk
+++ kona.mk
@@ -393,6 +393,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
